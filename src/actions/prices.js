6afdf6ef42acd8402e533af602e9fa086f31fde1
weini <weini.cxw@antgroup.com>
2021-01-25 21:28:10 +0800

    feat: actions/prices use coinmarketcap

--- src/actions/prices.js
+++ src/actions/prices.js
@@ -1,23 +1,33 @@
-const getProject = require("../services/qkl123/getProject");
+const getProject = require("../services/coinmarketcap/getProject");
 const sendToDing = require("../services/dingtalk/send");
 const { getDate } = require("../utils/date");
 
 const token = process.env.DD_TOKEN;
 
 (async () => {
-  const getContent = (data) => {
-    const priceColor = data.change[0] === "+" ? "#FF0000" : "#008000";
-    return `### ${data.name}
-#### 当前价：<font color="${priceColor}">${data.price}（${data.change}）</font>
-#### 最高/最低(24H)：${data.highPrice}/${data.lowPrice}
-#### 成交量(24H)：<font color="#9932CC">${data.deal}（换手率 ${data.dealRate}）</font>
-#### 总市值：${data.totalPrice}（占比 ${data.totalPriceRate}）
-#### 流通量/总量：${data.flow}（占比 ${data.flowRate}）`;
+  const getContent = (name, data) => {
+    const priceColor = data.isUp ? "#FF0000" : "#008000";
+    const changeBefore = data.isUp ? "+" : "-";
+    return `### ${name}
+##### 当前价：<font color="${priceColor}">${data.price}（${changeBefore}${data.change}）</font>
+##### 最高(24H)：${data.highPrice}
+##### 最低(24H)：${data.lowPrice}
+##### 成交量(24H)：<font color="#9932CC">${data.dealDisplay}</font>
+##### 换手率：<font color="#9932CC">${data.dealRate}</font>
+##### 总市值：${data.totalPriceDisplay}
+##### 市值占比：${data.totalPriceRate}
+##### 流通量：${data.flow}
+##### 总量：${data.maxFlow}
+##### 流通占比：${data.flowRate}`;
   };
 
-  const btc = await getProject("btc");
-  const eth = await getProject("eth");
-  const content = [getContent(btc), getContent(eth)].join("\n---\n");
+  const btc = await getProject("bitcoin");
+  const eth = await getProject("ethereum");
+
+  const content = [
+    getContent("比特币（BTC）", btc),
+    getContent("以太坊（ETH）", eth),
+  ].join("\n---\n");
 
   await sendToDing({
     token,
